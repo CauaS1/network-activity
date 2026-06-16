4a94b6334276ab61ee029a79d395fef43ac9be40
CauaS1 <semnet2404@gmail.com>
2026-06-16 20:57:58 -0300

    mariadb installed and working. insert data function inside db already working

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 from scapy.all import *
 from datetime import datetime
+import mariadb
 
+conn = mariadb.connect(
+    user="root",
+    password="1234567",
+    database="soc_lab"
+)   
+
+cursor = conn.cursor() # Tool that talks to the database
+
+def table_exists():
+    cursor.execute("""
+        SELECT EXISTS ( \
+        SELECT 1 FROM INFORMATION_SCHEMA.TABLES \
+        WHERE TABLE_SCHEMA = 'soc_lab' AND TABLE_NAME = 'network_events' \
+        ) AS table_exists
+    """)
+
+    res = cursor.fetchall()
+    res = res[0][0]
+
+    if res == 0:
+        cursor.execute("""CREATE TABLE network_events ( \
+        packet INT AUTO_INCREMENT PRIMARY KEY, \
+        timestamp VARCHAR(40), \
+        src_ip VARCHAR(15),
+        dst_ip VARCHAR(15),
+        protocol VARCHAR(8)) \
+    """)
 
 
 #packet = sniff(count=5)
@@ -8,7 +36,6 @@ from datetime import datetime
 
 def test():
     response = sr1(IP(dst="192.168.1.1") / ICMP())
-    print('data')
 
     # Fixing the timestamp from the response
     fixedTime = datetime.fromtimestamp(response.time)
@@ -21,18 +48,37 @@ def test():
     splitedResp.insert(0, fixedTime)
 
     # separating into variables the: timestamp, source ip, destination ip and protocol used
-    timestp = splitedResp[0]
-    src_ip = splitedResp[4]
-    dst_ip = splitedResp[6]
-    protoc = splitedResp[3]
+    timestp = str(splitedResp[0])
+    src_ip = str(splitedResp[4])
+    dst_ip = str(splitedResp[6])
+    protoc = str(splitedResp[3])
 
+    print('==========================================')
     print('timestamp ', timestp)
     print('source ', src_ip)
     print('destination ', dst_ip)
     print('protocvol ', protoc)
 
 
+    # Inserting data into the database
+        # You can use F-STRING to make the database unsafe for SQL Injection
+    cursor.execute("""
+        INSERT INTO network_events
+        (timestamp, src_ip, dst_ip, protocol)
+        VALUES (%s, %s, %s, %s)
+    """,
+    (timestp, src_ip, dst_ip, protoc)
+    )
+
+    conn.commit() # Don't forget to commit the changes into the db
+
+
+    
+
+
+table_exists()
 test()
+
     
 
 
