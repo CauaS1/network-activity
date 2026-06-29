705ce7cd2105d3a70d42446239e64704663932ee
CauaS1 <semnet2404@gmail.com>
2026-06-29 19:07:38 -0300

    removed useless code

--- log.py
+++ log.py
@@ -1,14 +1,5 @@
 from datetime import datetime
 import logging
-import mariadb
-
-conn = mariadb.connect(
-    user="root",
-    password="1234567",
-    database="soc_lab" # try deleting this line to see if i can create a logic to check the exitence of this db
-)   
-
-cursor = conn.cursor() # Tool that talks to the database
 
 logging.basicConfig(
     filename="test.log",
